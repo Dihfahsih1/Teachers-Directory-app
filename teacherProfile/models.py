ce2ff3b8f514fdee3e7681a52c9cac1f88a3c652
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-10 14:15:10 +0300

    create the models and adding there the tables required

--- teacherProfile/models.py
+++ teacherProfile/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100, null=False, unique=True)
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254, null=False, unique=True)
+    phone_number = models.CharField(max_length=20)
+    room_number = models.CharField(max_length=5)
+    image_name = models.ImageField(upload_to='pictures', blank=True,
+                                   default='pictures/default/default.png')
+
+    subjects = models.ManyToManyField(Subject)
