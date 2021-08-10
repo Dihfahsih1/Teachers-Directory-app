8ebaee69f6638151d6ed3eff0dba910042893a15
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-10 14:41:45 +0300

    changes

--- teacherProfile/forms.py
+++ teacherProfile/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.core.validators import FileExtensionValidator
-from .models import Profiles
 
 class ImageUploadForm(forms.Form):
     csv = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['csv'])])
