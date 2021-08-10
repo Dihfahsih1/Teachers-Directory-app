7bb16a55560464895713d9327197fedbf8e34c11
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-10 15:37:40 +0300

    place base template at the root folder of templates, register models in the admin to be used in the admin dashboard

--- teacherProfile/admin.py
+++ teacherProfile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile, Subject
+
+admin.site.site_header = "Teacher Directory Super Admin"
+admin.site.register(Profile)
+
+admin.site.register(Subject)
 # Register your models here.
