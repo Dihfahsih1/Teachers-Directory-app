a3e6a778cfc5675d9555060c6c38016717654083
Dihfahsih1 <dihfahsihm@gmail.com>
2021-08-18 08:27:20 +0300

    comment out the widgets on the upload form

--- csvs/forms.py
+++ csvs/forms.py
@@ -6,10 +6,9 @@ class CsvUploadForm(forms.ModelForm):
     class Meta:
         model = Csv
         fields = ('file_name','images_file',)
-        widgets = {
-            'file_name': forms.FileInput(attrs={'placeholder': 'upload CSV File'}),
-            'image_file': forms.TextInput(attrs={'placeholder': 'Upload Zipped file'}),
-        } 
+        # widgets = {'file_name': forms.TextField(attrs={'class': 'form-control','placeholder': 'upload CSV File'}),
+        #            'image_file': forms.TextInput(attrs={'placeholder': 'Upload Zipped file'}),
+        # } 
 
     #csv = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['csv'])])
     #zip = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['zip'])])
